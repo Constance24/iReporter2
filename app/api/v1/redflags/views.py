4ca4d6c6be68d9a64285b89fcda8618701f6704d
Constance24 <cmngila@gmail.com>
2018-12-06 11:31:24 +0300

    [finishes #162403902] updates changes on get incident

--- app/api/v1/redflags/views.py
+++ app/api/v1/redflags/views.py
@@ -11,10 +11,6 @@ class RedFlag(Resource):
         self.db = db
         self.id = len(db)+1
 
-
-    def get(self):
-        return db,200
-
     def post(self):
         data = request.get_json()
         
@@ -26,72 +22,10 @@ class RedFlag(Resource):
             "data" : "successfully created"
             }), 201)
 
+    def get(self):
+        return db,200
 
-
-    
-
-class RedFlagDelete(Resource):
-    def delete(self, id):
-        for incidence in db:
-            if incidence['id'] == id:
-                db.pop(id - 1)
-                success_message = {
-                'id' : id,
-                'message' : 'Red-Flag Deleted successfully'
-                }
-        return make_response(jsonify({
-            "status" : 204,
-            "data" : success_message
-            }), 204)
-
-    
-class ManageRedFlag(Resource):
-    def __init__(self):
-        self.db = db
-
-    def get(self, id):
-        incident = self.getById(id)
-
-        for item in db:
-            if item['id'] == id:
-                return item, 200
-
-        return item,200
-
-
-    def getById(self, id):
-       
-        pass
-
-    def patch(self, id):
-        incident = self.db.find(id)
-        if incident:
-            incident['location'] = request.json.get('location', incident['location'])
-            success_message = {
-                        'message' : 'updated location of redflag record'
-            }
-
-            return make_response(jsonify({
-                "status" : 201,
-                "data" : success_message
-            }), 201)
-
-
-def patch(self, id):
-        incident = self.db.find(id)
-        if incident:
-            incident['comment'] = request.json.get('comment', incident['comment'])
-            success_message = {
-                        'message' : 'updated comment on redflag record'
-            }
-
-            return make_response(jsonify({
-                "status" : 201,
-                "data" : success_message
-            }), 201)
 
 
     
 
-
-    
